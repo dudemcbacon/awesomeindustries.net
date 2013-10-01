1616037bf8d60d659812986ce8bb2fd6a9bdb717
Brandon Burnett <gentoolicious@gmail.com>
2013-09-30 19:43:42 -0700

    Commited changes I forgot to commit at work.
    Boy, this will be fun to figure out later.

--- js/awesome.js
+++ js/awesome.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  
- var randomnumber=Math.floor(Math.random()*9)
+ var randomnumber=Math.floor(Math.random()*3)
  $('.full').css({'background': "rgba(0, 0, 0, 0) url(./img/"+ randomnumber +".jpg) no-repeat fixed 50% 50% / cover padding-box border-box"});
 
 });
