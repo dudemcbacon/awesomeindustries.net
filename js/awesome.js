29464bca74c44fa1ef9dd6353b639ffa4cc5b044
Brandon Burnett <gentoolicious@gmail.com>
2013-10-02 14:08:35 -0700

    Lots of changes!

--- js/awesome.js
+++ js/awesome.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
  var randomnumber=Math.floor(Math.random()*3)
  $('.full').css({'background': "rgba(0, 0, 0, 0) url(./img/"+ randomnumber +".jpg) no-repeat fixed 50% 50% / cover padding-box border-box"});
 
+ wHeight = $(window).height();
+
+ $('#bacon').css({'height': wHeight - 51 - 50 })
 });
